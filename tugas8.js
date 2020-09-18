8d518b6ddb9a482d214c3ef7cbb8d8c18fa938dc
M. Ro'if <roifzubaidi5@gmail.com>
2020-09-19 05:02:30 +0700

    Tugas 8 JS
    Tugas ini kami kumpulkan untuk NIOMIC

---
+++ tugas8.js
@@ -0,0 +1,13 @@
+var data = {
+  nama: "M. Ro'if",
+  umur: 20,
+  kotaLahir: "Sumenep",
+};
+
+console.log("Nilai Awal: ");
+console.log(data);
+
+data.umur = 19;
+console.log("");
+console.log("Setelah Diubah (Umur): ");
+console.log(data);
